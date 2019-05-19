7f21ab3497dcee000630fd2f2cb92b8214d21adf
Marcin <marrin7@wp.pl>
2019-05-19 11:41:27 +0200

    final

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -24,5 +24,27 @@ public class TripManager {
 	public void remove(String name) {
 		tripList.remove(name);
 	}
-	
+
+	public Map<String, Trip> find(String keyword) {
+		
+		if (keyword == "") return tripList;
+		
+		Map<String, Trip>  findedTrips = new HashMap<String, Trip>();
+		
+		for  (String name : tripList.keySet()) {
+			if (name.contains(keyword)) {
+				findedTrips.put(name, tripList.get(name));
+			}
+			else if (tripList.get(name).getDescription().contains(keyword)) {
+				findedTrips.put(name, tripList.get(name));
+			} else {
+				for (Photo p : tripList.get(name).getPhotos()) {
+					if (p.getComment().contains(keyword)) {
+						findedTrips.put(name, tripList.get(name));
+					}
+				}
+			}
+		}
+		return findedTrips;
+	}
 }
--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.mwo;
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,7 +9,8 @@ import org.junit.Test;
 public class TripManagerTest {
 
 	TripManager tripManager;
-	Trip trip;
+	Trip trip, trip2;
+	Photo photo1, photo2;
 	
 	@Before
 	public void setUp() {
@@ -37,6 +39,57 @@ public class TripManagerTest {
 		assertEquals(1, tripManager.getTrips().size());
 		tripManager.remove(trip.getName());
 		assertEquals(0, tripManager.getTrips().size());
-		fail("chcemy zespuc");
+		//fail("chcemy zespuc");
 		}
+	
+	@Test
+	public void testFindTrip() throws Exception {
+		tripManager.add(trip);
+		HashMap<String,Trip> tripList = tripManager.getTrips();
+		assertEquals(tripList, tripManager.find(trip.getName()));
+	}
+	
+	@Test
+	public void testFindTripUsingEmptyString() throws Exception {
+		tripManager.add(trip);
+		tripManager.add(new Trip("beskidy", "beskid niski"));
+		tripManager.find("");
+		assertEquals(2, tripManager.getTrips().size());
+	}
+
+	@Test
+	public void testFindTripUsingEmptyStringOnEmptyTripList() throws Exception {
+		tripManager.find("");
+		assertEquals(0, tripManager.getTrips().size());
+	}
+	
+	@Before
+	public void setUp2() {
+		trip2 = new Trip("California Side", "USA");
+		photo1 = new Photo();
+		photo1.setComment("selfie1");
+		photo2 = new Photo();
+		photo2.setComment("selfie2");
+		trip2.addPhoto(photo1);
+		trip2.addPhoto(photo2);
+	}
+
+	@Test
+	public void testFindTripUsingKeywornInName() throws TripAlreadyExistsException {
+		tripManager.add(trip2);
+		assertEquals(1, tripManager.find("California").size());
+	}
+
+	@Test
+	public void testFindTripUsingKeywornInTripDesctiption() throws TripAlreadyExistsException {
+		tripManager.add(trip2);
+		assertEquals(1, tripManager.find("USA").size());
+	}
+
+	@Test
+	public void testFindTripUsingKeywornInPhotoComment() throws TripAlreadyExistsException {
+		tripManager.add(trip2);
+		assertEquals(1, tripManager.find("self").size());
+	}
+	
 }
